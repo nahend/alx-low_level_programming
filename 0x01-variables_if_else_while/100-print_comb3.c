f44ff64ea79cd1769d0cea74c53cc741b2b4e2c0
nahend <nahend>
2022-04-08 15:03:03 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print all posible conditions of two different digits,
+ *        in asceding order, separated by comma followed by space.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int digit1,digit2;
+
+for (digit1 = 0; digit2 < 9; digit++c)
+{
+for (digit2 = digit1 + 1; digit2 < 10; digit++c)
+{
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
+
+if (digit1==8 && digit2 == 9)
+
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
+}
