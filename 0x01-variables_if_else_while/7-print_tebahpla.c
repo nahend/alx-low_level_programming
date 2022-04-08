c9aa2e0e9367cec282092e413c2f068cb318e240
nahend <nahend>
2022-04-08 07:56:02 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print lower case alphabets backward 
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+
+for (c = 'z'; c >= 'a'; --c)
+putchar(c);
+
+putchar('\n');
+
+return (0);
+}
