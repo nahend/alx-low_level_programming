6589978de26c4f74cb91273fb8a5bd5180171f98
nahend <nahend>
2022-04-08 08:15:10 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * Description: Print all sing digit numbers  of base 10 
+ * starting from 0, followed by new line.
+ * Return: 0
+ */
+int main(void)
+{
+int c= 0;
+
+while (c < 10)
+}
+putchar(48 + c);
+if ( c !=9)
+{
+putchar(',')
+putchar(' ');
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
