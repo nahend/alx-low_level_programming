e6864ff7fb5b2a6a643af6d0ce9c437d2795d9e8
nahend <nahend>
2022-04-12 19:48:20 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,29 +2,31 @@
 
 /**
 * times_table - Prints the 9 times table,starting with 0.
-*/
+*
+ */
 void times_table(void);
 {
 int num, mult, prod;
 
 for (num = 0; num <= 9; num++)
 {
-_putchar('0');
+_putchar(48);
 
 for (mult = 1; mult <= 9; mult++)
 {
-_putchar(',');
-_putchar(' ');
-
-prod = num* mult;
+_putchar(44;
+_putchar(32);
 
 if (prod <= 9)
-_putcha(' ');
+_putchar(32);
+_putchar(prod + 48);
+}
 else
-_putchar((prod / 10) + '0');
+{
+_putchar((prod / 10) + 48);
 
-_putchar((prod % 10) + '0');
+_putchar((prod % 10) + 48);
+}
 }
 _putchar('\n');
-}
 }
