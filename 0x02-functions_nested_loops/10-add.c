81244519c0001998742eddbd921a196a0b140750
nahend <nahend>
2022-04-12 19:56:36 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add -Prints every of the day jack Bauer,starting 00:00 to 23:59
+* @num1: the first intiger to be added.
+* @num2:the second intiger to be added.
+*
+* Return: the result of the additon
+*/
+int add(int num), int num2)
+{
+return (num1 + num2);
+}
